08d5a2cd88f43386be717f13958a516318a53274
Glebka <gleb@1c02.ru>
2023-05-27 16:06:50 +0500

    kchay

--- SteamPriceArbitrage/Item.h
+++ SteamPriceArbitrage/Item.h
@@ -16,3 +16,35 @@ struct Item {
 void SerializeHead();
 
 void SerializeItem(Item& item);
+
+
+
+/*
+
+https://steamcommunity.com/market/search/render/?appid=730&norender=1&sort_column=name&sort_dir=desc&search_descriptions=0&count=100&start=20500
+
+success : true
+start : 20400
+pagesize : 100
+total_count : 20538
+
+results: [
+
+***
+{
+name : "AK-47 | Пустынный повстанец (Поношенное)"
+hash_name : "AK-47 | Wasteland Rebel (Well-Worn)"
+sell_listings : 21
+sell_price : 3714
+sell_price_text : "$37.14"
+app_icon : "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/730/69f7ebe2735c366c65c0b33dae00e12dc40edbe4.jpg"
+app_name : "Counter-Strike: Global Offensive"
+asset_description
+sale_price_text : "$35.53"
+}
+
+***
+
+]
+
+*/
